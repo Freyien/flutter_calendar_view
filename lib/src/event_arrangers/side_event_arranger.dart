58a0778f1585403c10477eaf273f67544b3365b9
faiyaz <faiyaz.s@simformsolutions.com>
2023-01-25 17:08:00 +0530

    Event till 12 am

--- lib/src/event_arrangers/side_event_arranger.dart
+++ lib/src/event_arrangers/side_event_arranger.dart
@@ -43,7 +43,9 @@ class SideEventArranger<T extends Object?> extends EventArranger<T> {
 
       while (concurrentEvents.isNotEmpty) {
         final event = concurrentEvents[currentEventIndex];
-        final end = event.endTime!.getTotalMinutes;
+        final end = event.endTime!.getTotalMinutes == 0
+            ? Constants.minutesADay
+            : event.endTime!.getTotalMinutes;
         sideEventData.add(_SideEventData(column: column, event: event));
         concurrentEvents.removeAt(currentEventIndex);
 
@@ -87,7 +89,11 @@ class SideEventArranger<T extends Object?> extends EventArranger<T> {
           left: slotWidth * (sideEvent.column - 1),
           right: slotWidth * (column - sideEvent.column),
           top: startTime.getTotalMinutes * heightPerMinute,
-          bottom: height - endTime.getTotalMinutes * heightPerMinute,
+          bottom: height -
+              (endTime.getTotalMinutes == 0
+                      ? Constants.minutesADay
+                      : endTime.getTotalMinutes) *
+                  heightPerMinute,
           startDuration: startTime,
           endDuration: endTime,
           events: [sideEvent.event],
